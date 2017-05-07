b70cad7e151976ffeb6765ae308d3e7938d41d1a
qzhuorui <qzhuorui@163.com>
2017-05-07 17:24:23 +0800

    adapter of chat

---
+++ app/src/main/java/explame/com/imooctestone/entity/ChatListData.java
@@ -0,0 +1,31 @@
+package explame.com.imooctestone.entity;
+
+/*
+ *      项目名：    ImoocTestOne
+ *      包名：       explame.com.imooctestone.entity
+ *      时间           2017/5/7.
+ *      创建者：    qzhuorui
+ *      描述：        对话列表de实体
+ */
+public class ChatListData {
+
+    private int type;
+    //文本
+    private String text;
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
--- app/src/main/java/explame/com/imooctestone/fragment/ButlerFragment.java
+++ app/src/main/java/explame/com/imooctestone/fragment/ButlerFragment.java
@@ -6,8 +6,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import explame.com.imooctestone.R;
+import explame.com.imooctestone.adapter.ChatListAdapter;
+import explame.com.imooctestone.entity.ChatListData;
 
 /*
  *      项目名：    ImoocTestOne
@@ -16,11 +23,68 @@ import explame.com.imooctestone.R;
  *      创建者：    qzhuorui
  *      描述：        TODO
  */
-public class ButlerFragment extends Fragment {
+public class ButlerFragment extends Fragment implements View.OnClickListener {
+
+    private ListView mChatListView;
+    private Button btn_left, btn_right;
+    private List<ChatListData> mList = new ArrayList<>();
+    private ChatListAdapter adapter;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_butler,null);
+        View view = inflater.inflate(R.layout.fragment_butler, null);
+        findview(view);
         return view;
+    }
+
+    private void findview(View view) {
+        mChatListView = (ListView) view.findViewById(R.id.mChatListView);
+        btn_left = (Button) view.findViewById(R.id.btn_left);
+        btn_left.setOnClickListener(this);
+        btn_right = (Button) view.findViewById(R.id.btn_right);
+        btn_right.setOnClickListener(this);
+
+        //设置适配器
+        ChatListAdapter adapter = new ChatListAdapter(getActivity(), mList);
+        mChatListView.setAdapter(adapter);
+
+        addLeftItem("你好，我是小管家");
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_left:
+                addLeftItem("左边");
+                break;
+            case R.id.btn_right:
+                addRightItem("右边");
+                break;
+        }
+    }
+
+    //添加左边文本
+    private void addLeftItem(String text) {
+        ChatListData date = new ChatListData();
+        date.setType(ChatListAdapter.VALUE_LEFT_TEXT);
+        date.setText(text);
+        mList.add(date);
+        //通知adapter刷新
+        adapter.notifyDataSetChanged();
+        //滚动到底部
+        mChatListView.setSelection(mChatListView.getBottom());
+    }
+
+    //添加左边文本
+    private void addRightItem(String text) {
+        ChatListData date = new ChatListData();
+        date.setType(ChatListAdapter.VALUE_RIGHT_TEXT);
+        date.setText(text);
+        mList.add(date);
+        //通知adapter刷新
+        adapter.notifyDataSetChanged();
+        //滚动到底部
+        mChatListView.setSelection(mChatListView.getBottom());
     }
 }
